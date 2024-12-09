b7e4c594ff096395e202a4674bd859bb5d425688
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-08 22:01:03 -0500

    Test: Created test function for an express server with a POST endpoint

---
+++ tests/server.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+
+const express = require('express');
+
+const app = require('../server'); // The server to be tested
+
+describe("POST / - Root Endpoint", () => {
+    
+    test("It should respond with the 'content' field from the JSON body", async () => {
+        const response = await request(app)
+            .post('/')
+            .send({ content: "Hello, world!" })
+            .set('Accept', 'application/json');
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({ content: "Hello, world!" });
+    });//end of test 
+
+    test("It should return a 400 error if 'content' field is missing", async () => {
+        
+        const response = await request(app)
+            .post('/')
+            .send({ noContent: "Oops!" })
+            .set('Accept', 'application/json');
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toEqual({ error: "'content' field is required" });
+   
+    });//end of test 
+
+});//end of describe 
